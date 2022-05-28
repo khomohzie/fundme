f2c60d428788b189957dff2739c59f1be5d14fc8
Daniel Komolafe <komozy2000@gmail.com>
2022-05-28 20:07:54 +0100

    smart contract written, compiled, tested, deployed

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CampaignFactory {
+    Campaign[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
+        uint256 approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    address public manager;
+    uint256 public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint256 public approversCount;
+    uint256 numRequests;
+    mapping(uint256 => Request) public requests;
+
+    constructor(uint256 minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(
+            msg.value > minimumContribution,
+            "The amount entered is less than the minimum contribution."
+        );
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public onlyManager {
+        // If requests were being treated as an array where we could construct a struct,
+        // we would say requests.push(newRequest) and then get the total number of requests
+        // using request.length BUT all that cannot happen because we cannot construct a
+        // struct with a mapping. So we have to manually increase the length of the array
+        // as shown in line 45 below.
+        Request storage newRequest = requests[numRequests++];
+
+        newRequest.description = description;
+        newRequest.value = value;
+        newRequest.recipient = recipient;
+        newRequest.complete = false;
+    }
+
+    function approveRequest(uint256 index) public {
+        Request storage request = requests[index];
+
+        require(
+            approvers[msg.sender],
+            "Permission denied! Only contributors can approve a specific payment request."
+        );
+
+        require(
+            !request.approvals[msg.sender],
+            "You have already voted to approve this request"
+        );
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint256 index) public onlyManager {
+        Request storage request = requests[index];
+
+        require(
+            request.approvalCount > (approversCount / 2),
+            "This request needs more approvals before it can be finalized."
+        );
+
+        require(!request.complete, "This request has already been finalized");
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
+}
